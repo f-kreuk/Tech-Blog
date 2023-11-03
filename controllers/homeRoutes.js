653d56eafb4a97f7d9d1f02c63255d99445f5576
Florian Kreuk <fkreuk@cartermario.com>
2023-11-03 14:32:53 -0400

    Making lots of stylistic changes to match the project requirements

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,6 +69,16 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -78,5 +88,7 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+
 
 module.exports = router;
